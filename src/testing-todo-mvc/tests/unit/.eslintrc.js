9d9642db1926ade743662208959d197a97f45af2
Michael Chilaka <chilakamichael001@gmail.com>
2019-06-20 20:18:21 +0100

    Tree structure for writing test

---
+++ src/testing-todo-mvc/tests/unit/.eslintrc.js
@@ -0,0 +1,22 @@
+// Tree Structure for Writing Tests
+// testing-todo-mvc/
+// README.md
+// babel.config.js
+// node_modules/
+// package.json
+// public/
+// src/
+// testing-todo-mvc/
+// ...
+// tests/
+// unit/
+// .eslintrc.js
+// example.spec.js
+
+// npm run test:unit
+
+module.export = {
+    env: {
+        jest: true
+    }
+}
---
+++ src/testing-todo-mvc/tests/unit/App.spec.js
@@ -0,0 +1,10 @@
+describe('App', () => {
+    it('passing test', () => {
+        expect(true).toBeTruthy();
+    });
+
+    it('failing test', () => {
+        expect(false).toBeTruthy();
+    });
+})
+
