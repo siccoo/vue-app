b13b6da3e61346f09f55f751f2496ebfa921e98f
Michael Chilaka <chilakamichael001@gmail.com>
2019-06-20 12:33:44 +0100

    NotFound.vue

---
+++ component/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+    <h3 class="subtitle has-text-white">
+        Sorry Pokemon page not found
+    </h3>
+</template>
+
+<script>
+export default {
+    name: "Not Found",
+};
+</script>
