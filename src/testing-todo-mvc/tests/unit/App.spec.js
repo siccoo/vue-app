e405e99b79aae12bf9df1961c55cca41f9378f49
Michael Chilaka <chilakamichael001@gmail.com>
2019-06-21 16:41:36 +0100

    installation of Vue Test Utils external library

--- src/testing-todo-mvc/tests/unit/App.spec.js
+++ src/testing-todo-mvc/tests/unit/App.spec.js
@@ -1,13 +1,23 @@
-import Vue from 'vue';
+// Installation and importation of Vue Test Utils external library
+// npm install @vue/test-utils --save-dev
+// import Vue from 'vue';
 import App from '@/App';
+import { shallowMount } from '@vue/test-utils';
 
 describe('App', () => {
     it('should render correct contents', () => {
-        const Constructor = Vue.extend(App);
-        const vm = new Constructor().$mount();
+        let wrapper = shallowMount(App);
+        // const Constructor = Vue.extend(App);
+        // const vm = new Constructor().$mount();
+        expect(wrapper.find('.title').text).toBe('todos');
+        expect(wrapper.find('.new-todo').element.placeholder).toBe('What needs to be done');
+    });
 
-        expect(vm.$el.querySelector('.title').textContent).toBe('todos');
-        expect(vm.$el.querySelector('.new-todo').placeholder).toBe('What needs to be done');
+    it('should set correct data', () => {
+        const initialData = App.data();
+
+        expect(initialData.todos).toEqual([]);
+        expect(initialData.newTodo).toEqual('');
     });
 });
 
